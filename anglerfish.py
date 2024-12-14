884379457a35cbc24470d357560b41e7aef05689
Shiwei Wang <shiweiwtest@gmail.com>
2024-12-14 15:06:59 -0800

    add comments

--- anglerfish.py
+++ anglerfish.py
@@ -6,18 +6,23 @@ from pybricks.robotics import DriveBase
 
 def run_anglerfish(hub: PrimeHub, robot: DriveBase, lmotor: Motor):
     robot.settings(straight_speed=300, turn_rate=100)
+    # go straight off base. 
+    # close but not touch artificial habitab
     robot.straight(200)
+    # turn right a bit to head to right of anglerfish
     robot.turn(-30)
     robot.straight(500)
+    # turn right toward the cold sea 
     robot.turn(-52)
+    # go to cold sea so octopus land in the sea
     robot.straight(160)
+    # come back a bit to leave octopus in the sea
     robot.straight(-150)
+    # turn left to align with anglerfish
     robot.turn(40)
+    # go straight to push the latch
     robot.straight(600)
-    robot.turn(40)
-    robot.straight(220)
-    robot.turn(40)
-    robot.straight(900)
+    # stop there. end of game
 
 
 
--- raisethefoopymast.py
+++ raisethefoopymast.py
@@ -4,17 +4,28 @@ from pybricks.robotics import DriveBase
 
 def raiseFoopyMast(hub: PrimeHub, robot: DriveBase):
     robot.settings(straight_speed=300)
+    # go forward and stop before nursury
     robot.straight(400)
+    # turn right a bit to avoid nursery
     robot.turn(-25)
+    # forward diagonally to aligh with mast
     robot.straight(125)
+    # turn right toward mast
     robot.turn(-65)
     robot.settings(straight_speed=350, turn_rate=100)
+    # get close to mask and stop before touching it
     robot.straight(100) 
+    #  make it slower to avoid over flip mast
     robot.settings(straight_speed=100)
+    # forward to lift mast
     robot.straight(150)
+    # backward to leave mast alone
     robot.straight(-140)
+    # turn left to align with base direction
     robot.turn(45)
+    # raise speed
     robot.settings(straight_speed=450)
+    # back to base
     robot.straight(-600)
 
 
